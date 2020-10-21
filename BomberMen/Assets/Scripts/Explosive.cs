cde5ea9c2850a5cf6812d2ffd96b364dcb364533
Yaros.S <sergeyya@playtika.com>
2020-10-21 12:22:43 +0300

    bomber man

---
+++ BomberMen/Assets/Scripts/Explosive.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Explosive : MonoBehaviour
+{
+    [SerializeField] private float radius;
+    [SerializeField] private float delay;
+    [SerializeField] private LayerMask bombMask;
+    
+    
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(delay);
+        Boom();
+    }
+
+    private void Boom()
+    {
+        RayCast(Vector2.down * radius);
+        RayCast(Vector2.up * radius);
+        RayCast(Vector2.left * radius);
+        RayCast(Vector2.right * radius);
+        Destroy(gameObject);
+    }
+    private void RayCast(Vector2 dir)
+    {
+        
+        var hits = Physics2D.RaycastAll(transform.position, dir, 1f,bombMask);
+        foreach (var hit in hits)
+        {
+            Debug.Log(hit.collider.name);
+            if (hit.collider.CompareTag("Destroyed") || hit.collider.CompareTag("Player"))
+            {   
+              
+                Destroy(hit.collider.gameObject);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
---
+++ BomberMen/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private LayerMask raycastMask;
+    [SerializeField] private GameObject exp;
+    
+    private bool isInMovement;
+
+
+    void Update()
+    {
+        if (isInMovement)
+            return;
+        
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            MovePlayerTo(Vector2.left);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            MovePlayerTo(Vector2.right);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            MovePlayerTo(Vector2.up);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            MovePlayerTo(Vector2.down);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var obj = Instantiate(exp, transform.position, quaternion.identity);
+        }
+       
+    }
+
+ 
+
+
+    private void MovePlayerTo(Vector2 dir)
+    {
+        if (Raycast(dir))
+            return;
+        isInMovement = true;
+
+        var pos = (Vector2) transform.position + dir;
+        transform.DOMove(pos, 0.5f).OnComplete(() =>
+        {
+            isInMovement = false;
+        });
+    }
+
+    private bool Raycast(Vector2 dir)
+    {    
+        var hit = Physics2D.Raycast(transform.position, dir, 1f, raycastMask);
+        return hit.collider != null;
+    }
+
+    
+}
